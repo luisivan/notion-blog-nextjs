3da69b12fc823cdb0f8162345574bf0e9ae69e02
Luis Cuende <me@luisivan.net>
2021-10-19 14:58:00 +0100

    Fixes

--- next.config.js
+++ next.config.js
@@ -1,38 +1,36 @@
-/*const withTM = require('next-transpile-modules')([
-  'fetch-meta-tags',
-  'fetch-blob',
-  'stream',
-])*/
+const withTM = require('next-transpile-modules')(['fetch-meta-tags'])
 const { withPlaiceholder } = require('@plaiceholder/next')
 
-module.exports = withPlaiceholder({
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        notionhq: false,
+module.exports = withTM(
+  withPlaiceholder({
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        config.resolve.fallback = {
+          notionhq: false,
+        }
       }
-    }
 
-    return config
-  },
-  rewrites() {
-    return [
-      {
-        source: '/rss.xml',
-        destination: '/api/feed/rss2',
-      },
-      {
-        source: '/atom.xml',
-        destination: '/api/feed/atom1',
-      },
-      {
-        source: '/feed.json',
-        destination: '/api/feed/json1',
-      },
-    ]
-  },
-  images: {
-    domains: ['s3.us-west-2.amazonaws.com'],
-  },
-  experimental: { esmExternals: true },
-})
+      return config
+    },
+    rewrites() {
+      return [
+        {
+          source: '/rss.xml',
+          destination: '/api/feed/rss2',
+        },
+        {
+          source: '/atom.xml',
+          destination: '/api/feed/atom1',
+        },
+        {
+          source: '/feed.json',
+          destination: '/api/feed/json1',
+        },
+      ]
+    },
+    images: {
+      domains: ['s3.us-west-2.amazonaws.com'],
+    },
+    experimental: { esmExternals: true },
+  })
+)
