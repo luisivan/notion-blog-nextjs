4005a8b0dd8af808c276035a652c62f4f340f421
Luis Cuende <me@luisivan.net>
2021-12-20 13:53:01 +0000

    Fix navbar responsive

--- components/nav.js
+++ components/nav.js
@@ -4,11 +4,11 @@ import config from '../config'
 import logo from '../public/logo.jpg'
 
 export const Nav = () => (
-  <header className="container mx-auto max-w-screen-lg flex justify-between py-5 font-display">
+  <header className="container mx-auto max-w-screen-lg flex justify-between py-5 font-display text-sm md:text-base">
     <Link href="/" passHref>
       <a className="flex items-center">
         <Image src={logo} alt="" width="48" height="48" />
-        <h1 className="text-xl ml-4 bg-clip-text">{config.name}</h1>
+        <h1 className="text-sm md:text-xl ml-4 bg-clip-text">{config.name}</h1>
       </a>
     </Link>
     <nav className="flex justify-between items-center">
