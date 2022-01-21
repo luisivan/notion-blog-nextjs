e71776a08d4ead65b9a8ef27a6125cc10008043d
Luis Cuende <me@luisivan.net>
2022-01-21 14:49:41 +0000

    Providing a uniform URL for Notion images hosted on AWS, so that Vercel doesn't have to cache different ones each time

--- pages/api/imageproxy.js
+++ pages/api/imageproxy.js
@@ -1,5 +1,13 @@
+import { getBlock } from '../../lib/notion'
+
 export default async function proxy(req, res) {
-  const url = decodeURIComponent(req.query.url)
+  let url = ''
+  if (req.query.url) {
+    url = decodeURIComponent(req.query.url)
+  } else if (req.query.id) {
+    const block = await getBlock(req.query.id)
+    url = block.image.file.url
+  }
   const result = await fetch(url)
   const body = await result.body
   body.pipe(res)
